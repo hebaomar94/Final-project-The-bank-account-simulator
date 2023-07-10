b82cb3f4dc897b4258615b758345f3aed86d5127
Heba Omar <97067717+hebaomar94@users.noreply.github.com>
2023-07-10 16:18:19 +0300

    Update coursera.swift

--- coursera.swift
+++ coursera.swift
@@ -33,3 +33,74 @@ repeat {
     let numberPadKey = Int.random(in: 1...3)
     welcomCustomer.makeAccount(numberPadKey: numberPadKey)
 } while welcomCustomer.accountType == ""
+
+
+struct BankAccount {
+    //Creating stored properties
+    var debitBalance = 0
+    var creditBalance = 0
+    let creditLimit = 100
+    //Creating computed properties
+    var  debitBalanceInfo: String{
+        "Debit balance: $\(debitBalance)"
+    }
+
+    var  availableCredit: Int {
+        creditLimit + creditBalance
+    }
+    
+    var creditBalanceInfo: String {
+        return "Available credit:\(availableCredit)"
+    }
+    // Implement the deposit operation for debit and credit bank accounts
+    mutating func debitDeposit(_ amount: Int) {
+          debitBalance += amount
+          print("Debit deposit: $\(amount). \(debitBalanceInfo)")
+      }
+    
+    //Implement the withdrawal operation for debit and credit bank accounts
+    mutating func debitWithdraw (_ amount: Int){
+        if amount > debitBalance {
+            print("Insufficient funds to withdraw $\(amount). \(debitBalanceInfo)")
+               } else {
+                   debitBalance -= amount
+                   print("Debit withdraw: $\(amount). \(debitBalanceInfo)")
+               
+        }
+    }
+    
+    //Credit deposit
+
+    mutating func creditDeposit (_ amount: Int){
+        creditBalance += amount
+        print("Credit deposit: $\(amount). \(creditBalanceInfo)")
+        if creditBalance == 0 {
+                    print("Paid off credit balance.")
+                } else if creditBalance > 0 {
+                    print("Overpaid credit balance.")
+                }
+    }
+    
+    mutating func creditWithdraw (_ amount: Int){
+        if amount > availableCredit {
+            print ("Insufficient credit to withdraw: \(amount). \(creditBalanceInfo)")
+        } else {
+            print ("Credit withdraw: \(amount) \(creditBalanceInfo)")
+        }
+    }
+    
+    
+}
+//Create a bank account instance
+var bankAccount = BankAccount()
+print(bankAccount.debitBalanceInfo)
+bankAccount.debitDeposit(100)
+bankAccount.debitWithdraw(20)
+bankAccount.debitWithdraw(81)
+print(bankAccount.creditBalanceInfo)
+bankAccount.creditWithdraw(101)
+bankAccount.creditWithdraw(100)
+bankAccount.creditDeposit(50)
+bankAccount.creditDeposit(50)
+bankAccount.creditDeposit(100)
+
